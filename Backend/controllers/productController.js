c861da0eb1a435e4d3a836a73c8e4eb30af473b9
ABay23 <alejandrobay23@gmail.com>
2023-03-16 00:00:47 -0400

    Add Products to cloudinary

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler')
 const Product = require('../models/productModel')
 const { fileSizeFormatter } = require('../utility/fileUpload')
+const cloudinary = require('cloudinary').v2
 
 const createProduct = asyncHandler(async (req, res) => {
   const { name, sku, category, price, quantity, description } = req.body
@@ -15,9 +16,22 @@ const createProduct = asyncHandler(async (req, res) => {
   let fileData = {}
 
   if (req.file) {
+    let uploadedFile
+
+    try {
+      uploadedFile = await cloudinary.uploader.upload(
+        //* File Path
+        req.file.path,
+        { folder: 'inventory-2.0', resource_type: 'image' }
+      )
+    } catch (error) {
+      res.status(500)
+      throw new Error('Image could not be uploaded')
+    }
+
     fileData = {
       fileName: req.file.originalname,
-      filePath: req.file.path,
+      filePath: uploadedFile.secure_url,
       filetype: req.file.mimetype,
       fileSise: fileSizeFormatter(req.file.size, 2),
     }
@@ -36,6 +50,14 @@ const createProduct = asyncHandler(async (req, res) => {
   res.status(201).json(product)
 })
 
+//* Get all Product
+
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({ user: req.user.id })
+  res.status(200).json(products).sort('-createdAt')
+})
+
 module.exports = {
   createProduct,
+  getProducts,
 }
