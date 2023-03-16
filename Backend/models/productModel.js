d757a9cdb9d301f876791d65d4301ae3fc3524f9
ABay23 <alejandrobay23@gmail.com>
2023-03-16 13:15:58 -0400

    Issue with remove not command

--- Backend/models/productModel.js
+++ Backend/models/productModel.js
@@ -1,10 +1,65 @@
+// const mongoose = require('mongoose')
+
+// const productSchema = mongoose.Schema(
+//   {
+//     user: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       require: true,
+//       ref: 'User',
+//     },
+//     name: {
+//       type: String,
+//       required: [true, 'Please add a name'],
+//       trim: true,
+//     },
+//     sku: {
+//       type: String,
+//       required: true,
+//       default: 'SKU',
+//       trim: true,
+//     },
+//     category: {
+//       type: String,
+//       required: [true, 'Please add a category'],
+//       trim: true,
+//     },
+//     quantity: {
+//       type: String,
+//       required: [true, 'Please add a quantity'],
+//       trim: true,
+//     },
+//     price: {
+//       type: String,
+//       required: [true, 'Please add a price'],
+//       trim: true,
+//     },
+//     description: {
+//       type: String,
+//       required: [true, 'Please add a description'],
+
+//       trim: true,
+//     },
+//     image: {
+//       type: Object,
+//       default: {},
+//     },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// )
+
+// const Product = mongoose.model('Product', productSchema)
+
+// module.exports = Product
+
 const mongoose = require('mongoose')
 
 const productSchema = mongoose.Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
-      require: true,
+      required: true,
       ref: 'User',
     },
     name: {
@@ -49,5 +104,4 @@ const productSchema = mongoose.Schema(
 )
 
 const Product = mongoose.model('Product', productSchema)
-
 module.exports = Product
