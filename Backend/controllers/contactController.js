be2090685f7e8e299af367bb7450fad850309af9
ABay23 <alejandrobay23@gmail.com>
2023-03-21 15:13:29 -0400

    Send message

--- Backend/controllers/contactController.js
+++ Backend/controllers/contactController.js
@@ -1,7 +1,32 @@
-const asyncHandler = require('async-handler')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+const emailSender = require('../utility/sendEmail')
 
 const contactUs = asyncHandler(async (req, res) => {
-  res.send('Contact us.')
+  const { subject, message } = req.body
+
+  const user = await User.findById(req.user._id)
+
+  if (!user) {
+    res.status(400)
+    throw new Error('User Not Found')
+  }
+  if (!subject || !message) {
+    res.status(400)
+    throw new Error('Please add Subject and a Message')
+  }
+
+  const send_to = process.env.EMAIL_USER
+  const send_from = process.env.EMAIL_USER
+  const reply_to = user.email
+
+  try {
+    await emailSender(subject, message, send_to, send_from, reply_to)
+    res.status(200).json({ success: true, message: 'Email Sent' })
+  } catch (error) {
+    res.status(500)
+    throw new Error('Email not sent, please try again.')
+  }
 })
 
 module.exports = { contactUs }
