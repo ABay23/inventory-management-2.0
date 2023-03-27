0ed31718ba9895c8ebdfe5678c7b4f9a9d3f4ce6
ABay23 <alejandrobay23@gmail.com>
2023-03-27 15:57:19 -0400

    New Templates Tailwind css and Homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import Home from './pages/Home/Home'
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </Router>
+    <>
+      <div className=' container'>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </Router>
+      </div>
+    </>
   )
 }
 
