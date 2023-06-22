4b4cb069f5ecde65aba0a67689d811bd3d33b2b1
ABay23 <alejandrobay23@gmail.com>
2023-06-21 21:59:08 -0400

    Updated flex description and space

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
             </p>
           </div>
           <div className='flex flex-wrap -m-4'>
-            <div className='xl:w-1/3 md:w-1/2 p-4'>
+            <div className='xl:w-1/3 md:w-1/2 p-4 h-52'>
               <div className='border border-gray-700 border-opacity-75 p-6 rounded-lg'>
                 <div className='w-10 h-10 inline-flex items-center justify-center rounded-full bg-gray-800 text-blue-400 mb-4'>
                   <svg
@@ -35,7 +35,9 @@ const Home = () => {
                 </h2>
                 <p className='leading-relaxed text-base'>
                   Take inventory and set pars from any device. Manage the
-                  details like case size, order amount,
+                  details like case size, order amount.
+                  <br />
+                  You can customize the inventory to your needs.
                 </p>
               </div>
             </div>
@@ -61,7 +63,8 @@ const Home = () => {
                 </h2>
                 <p className='leading-relaxed text-base'>
                   Eliminate IT costs and concerns associated with maintaining
-                  and upgrading separate applications.
+                  and upgrading separate applications. Authentication is handled
+                  by the system.
                 </p>
               </div>
             </div>
